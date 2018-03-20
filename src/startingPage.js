e53a7afde35cb7dbdd35c3c9735ead0790376f2f
tiaoshi <lonqer@gmail.com>
2018-03-20 12:28:26 +0800

    Tell users that Edge is not supported

--- src/startingPage.js
+++ src/startingPage.js
@@ -1,14 +1,18 @@
-(function(angular) {
+(function(document, angular) {
 	'use strict';
 
 	const startingPageTemplate = `
 <div class="sign-in">
-    <div class="header">深圳外国语学校<br />填者荣耀</div>
-    <div>
+	<div class="header">深圳外国语学校<br />填者荣耀</div>
+	<div ng-if="!$ctrl.isCompatible">
+		当前浏览器不支持本网页程序所需的特性。
+		<br />请换其它较新的浏览器来打开。
+	</div>
+    <div ng-if="$ctrl.isCompatible">
         <label for="playername">姓名</label>
         <input id="playername" ng-model="$ctrl.playername" ng-disabled="$ctrl.isPlayernameSet" type="text">
     </div>
-    <div class="footer">
+    <div ng-if="$ctrl.isCompatible" class="footer">
         <button ng-if="!$ctrl.isPlayernameSet" ng-click="$ctrl.setPlayername()">提交</button>
         <button ng-if="$ctrl.isPlayernameSet" ng-click="$ctrl.onStartClicked()">开始</button>
     </div>
@@ -17,11 +21,17 @@
 	angular.module('app').component('startingPage', {
 		template: startingPageTemplate,
 		controller: [
+			'$element',
 			'$state',
 			'GameService',
-			function StartingPageController($state, GameService) {
+			function StartingPageController($element, $state, GameService) {
 				this.$onInit = () => {
 					this.isPlayernameSet = false;
+
+					if (document.documentMode || /Edge/.test(navigator.userAgent))
+						this.isCompatible = false
+					else
+						this.isCompatible = true
 				};
 				this.setPlayername = () => {
 					GameService.setPlayer(this.playername);
@@ -34,4 +44,4 @@
 			}
 		]
 	});
-})(angular);
+})(document, angular);
