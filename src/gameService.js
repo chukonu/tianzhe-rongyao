2ad2de79682ffc63c51fa9fec1c7bd36646382e8
tiaoshi <lonqer@gmail.com>
2018-03-28 19:14:10 +0800

    imported new maps

--- src/gameService.js
+++ src/gameService.js
@@ -1,11 +1,23 @@
 (function (angular) {
     'use strict'
-    angular.module('app').service('GameService', ['r1s1', 'r1s2', 'r1s3', function GameService(r1s1, r1s2, r1s3) {
-        const stages = [r1s1, r1s2, r1s3]
+    angular.module('app').service('GameService', ['r1s1', 'r1s2', 'r1s3', 'r5s1', 'r5s2', 'r5s3', 'r6s1', 'r6s2', 'r6s3', function GameService(r1s1, r1s2, r1s3, r5s1, r5s2, r5s3, r6s1, r6s2, r6s3) {
+        const round1 = [r1s1, r1s2, r1s3]
+        const round5 = [r5s1, r5s2, r5s3]
+        const round6 = [r6s1, r6s2, r6s3]
+        const rounds = [round1, round5, round6]
+        let currentRound = 0
+        let stages = rounds[currentRound]
 
         this.playerName = null
         this.timeStarted = null
         this.timeEnded = null
+
+        this.setCurrentRound = num => {
+            currentRound = num
+            stages = rounds[currentRound]
+        }
+        this.getCurrentRound = () => currentRound
+        this.getAllRounds = () => ['游戏１', '游戏５', '游戏６']
         
         this.setPlayer = playerName => this.playerName = playerName
         this.getPlayer = () => this.playerName
